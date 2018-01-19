421f8a41d445f53c7c4e53f17efe1d8bc5f3bc93
Stig Ofstad <stigofstad@gmail.com>
2018-01-19 13:08:42 +0100

    Update sensu-backup_hosts.sh

--- sensu-backup_hosts.sh
+++ sensu-backup_hosts.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-PUPPETMASTER_FQDN="puppet.sdp.statoil.no"
+PUPPETMASTER_FQDN="puppet.???.no"
 PUPPETMASTER_API_PORT="8080"
-TARGETS_LOC="/root/SDP-backup/targets.csv"
+TARGETS_LOC="/home/backup/SDP-backup/targets.csv"
+export NO_PROXY=$PUPPETMASTER_FQDN
 
 # Query the Puppet masters API for a json-list of all managed nodes.
 # Format the json output to an whitespace separated array containing only the alternative hostnames.
@@ -11,10 +12,16 @@ HOST_ARRAY=($(curl --silent "http://$PUPPETMASTER_FQDN:$PUPPETMASTER_API_PORT/pd
  cut --delimiter ":" --fields 2 |
  tr --delete [:blank:]\", ))
 
+# Nested for-loop. Checks if any hostname matches any script parameters on wildcard.
 # Loop through the scripts parameter list and remove any occurrences in the array.
-for i in "$@"; do
-    HOST_ARRAY=($(echo ${HOST_ARRAY[@]} | sed "s/$i//g"))
+for i in ${HOST_ARRAY[@]}; do
+    for c in "$@"; do
+        if [[ "$i" == *"$c"* ]]; then
+            HOST_ARRAY=($(echo ${HOST_ARRAY[@]} | sed "s/$i//g"))
+        fi
+    done
 done
+
 
 # Loop through the array and grep for the hostname in the 'targets.csv' file used in the backup-job.
 for i in ${HOST_ARRAY[@]}; do
@@ -25,12 +32,12 @@ for i in ${HOST_ARRAY[@]}; do
 done
 
 if [ -z "$NO_BACKUP" ]; then
-      echo "All hostnames found in targets.csv"
+      echo "All hostnames found in targets.csv. Something is likely to be backedup..."
       exit 0
 else
     echo -e "BACKUP WARNING - These hosts are not mentioned in the 'targets.csv'-file:\n
     $NO_BACKUP\n
-    Consider adding them. You can exclude the host from this sensu-check by adding the
-    hostname to the check command as parameters."
+    You can add them at 'https://git.???/targets.csv'. You can exclude the host from this sensu-check by adding the
+    hostname to the check command as parameters. The command is found at 'PUPPET/environment/data/nodes/backup01'."
     exit 1
 fi
