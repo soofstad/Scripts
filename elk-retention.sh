5736836baac20d761760ac2c25f0c2e430d53651
Stig Oskar Myhre Ofstad <stoo@statoil.com>
2018-02-27 16:23:17 +0100

    Make curl silent ex errors

--- elk-retention.sh
+++ elk-retention.sh
@@ -3,14 +3,17 @@ ELK_ENDPOINT="localhost:9200/"
 ELK_INDICES="filebeat-"
 ELK_CLOSE_TIME_MONTHS="2"
 ELK_DELETE_TIME_MONTHS="6"
+CURLARGS="-f -s -S"
 
 # Close INDICES
 CLOSE_DATE=$(date -d "-${ELK_CLOSE_TIME_MONTHS} month" +%Y.%m.)
 echo -e "Closing indices using this filter:\n${ELK_ENDPOINT}${ELK_INDICES}${CLOSE_DATE}*"
-curl -XPOST "${ELK_ENDPOINT}${ELK_INDICES}${CLOSE_DATE}*/_flush"
-curl -XPOST "${ELK_ENDPOINT}${ELK_INDICES}${CLOSE_DATE}*/_close"
+curl $CURLARGS -XPOST "${ELK_ENDPOINT}${ELK_INDICES}${CLOSE_DATE}*/_flush"
+curl $CURLARGS -XPOST "${ELK_ENDPOINT}${ELK_INDICES}${CLOSE_DATE}*/_close"
 
 # Delete INDICES
 DELETE_DATE=$(date -d "-${ELK_DELETE_TIME_MONTHS} month" +%Y.%m.)
+echo ""
 echo -e "Deleting indices using this filter:\n${ELK_ENDPOINT}${ELK_INDICES}${DELETE_DATE}*"
-curl -XDELETE "${ELK_ENDPOINT}${ELK_INDICES}${CLOSE_DATE}*"
+curl $CURLARGS -XDELETE "${ELK_ENDPOINT}${ELK_INDICES}${CLOSE_DATE}*"
+
