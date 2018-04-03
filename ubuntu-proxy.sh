2cf6838e7c8254f6fe4ec4873d16ccc7eda704f2
Stig Oskar Myhre Ofstad <stoo@statoil.com>
2018-04-03 08:04:30 +0200

    Added the ZFSnap cron-script

--- ubuntu-proxy.sh
+++ ubuntu-proxy.sh
@@ -1,33 +1,32 @@
 #! /bin/bash
 HTTP_PROXY_HOST=www-proxy.statoil.no
 HTTP_PROXY_PORT=80
-HTTPS_PROXY_HOST=proxy.example.com
+HTTPS_PROXY_HOST=www-proxy.statoil.no
 HTTPS_PROXY_PORT=80
 
-
 http_proxy="http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT/"
-HTTP_PROXY="http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT/"
-https_proxy="http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT/"
-HTTPS_PROXY="http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT/"
+HTTP_PROXY=$http_proxy
+https_proxy="http://$HTTPS_PROXY_HOST:$HTTPS_PROXY_PORT/"
+HTTPS_PROXY=$https_proxy
 
+if $1 = 'set'; then
+    gsettings set org.gnome.system.proxy mode manual
+    gsettings set org.gnome.system.proxy.http host "$HTTP_PROXY_HOST"
+    gsettings set org.gnome.system.proxy.http port "$HTTP_PROXY_PORT"
+    gsettings set org.gnome.system.proxy.https host "$HTTPS_PROXY_HOST"
+    gsettings set org.gnome.system.proxy.https port "$HTTPS_PROXY_PORT"
 
-gsettings set org.gnome.system.proxy mode manual
-gsettings set org.gnome.system.proxy.http host "$HTTP_PROXY_HOST"
-gsettings set org.gnome.system.proxy.http port "$HTTP_PROXY_PORT"
-gsettings set org.gnome.system.proxy.https host "$HTTPS_PROXY_HOST"
-gsettings set org.gnome.system.proxy.https port "$HTTPS_PROXY_PORT"
-
-sudo sed -i.bak '/http[s]::proxy/Id' /etc/apt/apt.conf
-sudo tee -a /etc/apt/apt.conf <<EOF
-Acquire::http::proxy "http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT/";
-Acquire::https::proxy "http://$HTTPS_PROXY_HOST:$HTTPS_PROXY_PORT/";
+    tee /etc/apt/apt.conf <<EOF
+Acquire::http::proxy "${http_proxy}";
+Acquire::https::proxy "${https_proxy}";
 
 EOF
 
-sudo sed -i.bak '/http[s]_proxy/Id' /etc/environment
-sudo tee -a /etc/environment <<EOF
-http_proxy="http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT/"
-http_proxy="http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT/"
-https_proxy="http://$HTTPS_PROXY_HOST:$HTTPS_PROXY_PORT/"
-https_proxy="http://$HTTPS_PROXY_HOST:$HTTPS_PROXY_PORT/"
-EOF
+grep -qF -- "http_proxy="${http_proxy}
+HTTP_PROXY="${http_proxy}"
+https_proxy="${https_proxy}"
+HTTPS_PROXY="${https_proxy}"
+
+elif $1 = 'unset'; then
+    echo 'UNseting'
+fi
---
+++ zfsnap-cron.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# A script meant to be run as a cron job to manage snapshots of a system running ZFS
+# Prerequisites
+#       - A ZFS resource (pool or filesystem)
+#       - The zfsnap utility (https://github.com/zfsnap/zfsnap)
+# Path to zfsnap.sh
+ZFSNAP_INSTALL_PATH="/zfs/filesystem/zfsnap/sbin"
+# Include zfsnap in PATH
+PATH="$PATH:$ZFSNAP_INSTALL_PATH"
+# The zfs resource to perform recursive snapshots of
+ZFS_RESOURCE="tank"
+# Set the Time To Live for the different snapshot-categories.
+# y = years, m = months, w = weeks, d = days, h = hours, M = minutes, s = seconds
+TTL_DAILY="7d"
+TTL_WEEKLY="5w"
+TTL_MONTHLY="6m"
+
+# Call the same script from cron with 3 different arguments. To minimize "config" in crontab.
+if [ "$1" = "daily" ]; then
+    # Create a snapshot with the 'daily' TTL with a prefix. Recursively on the ZFS resource.
+    zfsnap.sh snapshot -v -z -a $TTL_DAILY -p 'DAILY-' -r $ZFS_RESOURCE
+    # Also destroy all expired snapshots daily
+    zfsnap.sh destroy -v -p 'DAILY-' -p 'WEEKLY-' -p 'MONTHLY-' -r $ZFS_RESOURCE
+elif [ "$1" = "weekly" ]; then
+    zfsnap.sh snapshot -v -z -a $TTL_WEEKLY -p 'WEEKLY-' -r $ZFS_RESOURCE
+elif [ "$1" = "monthly" ]; then
+    zfsnap.sh snapshot -v -z -a $TTL_MONTHLY -p 'MONTHLY-' -r $ZFS_RESOURCE
+else
+    echo "Error: Requires atleast one argument."
+    exit 1
+fi
+exit 0
